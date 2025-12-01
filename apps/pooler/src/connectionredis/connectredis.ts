5592bada527f7fd7318817a51cbbaedb8939b0db
Aditi Shukla <aditishukla1676@gmail.com>
2025-12-01 20:03:46 +0530

    feat: add Redis connection utility file

--- apps/pooler/src/connectionredis/connectredis.ts
+++ apps/pooler/src/connectionredis/connectredis.ts
@@ -1,15 +1,14 @@
-import { createClient, type RedisClientType } from "redis";
+import { createClient,type RedisClientType } from "redis";
 
-export const pub: RedisClientType = createClient({
-    url: process.env.REDIS_URL||"redis://localhost:6379",
+export const pub: RedisClientType=createClient({
+    url:process.env.REDIS_URL || "redis://localhost:6379"
 });
 
-pub.on("error",(err) => console.error("Redis Client Error.",err));
+pub.on("error",(err)=> console.error("Redis Client Error.",err));
 
 export async function connectRedis() {
-    if (!pub.isOpen) {
+    if(!pub.isOpen){
         await pub.connect();
         console.log("Connected to Redis");
-
     }
 }
