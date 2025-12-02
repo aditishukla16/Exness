0a4a94ecfd7bcafbfe91667991fcef9b0e3eb62a
Aditi Shukla <aditishukla1676@gmail.com>
2025-12-02 11:06:37 +0530

    feat: add websocket price scaling and Redis publishing logic

--- apps/pooler/src/router/router.ts
+++ apps/pooler/src/router/router.ts
@@ -1,24 +1,24 @@
 import type { Data } from "../type";
-import { pub } from "../connectionredis/connectredis";
+import {pub} from "../connectionredis/connectredis";
 
 const bidPriceIncrementRate = 0.0005;
 const askPriceDecrementRate = 0.0005;
 
-export async function scalewebsocket(data:Data) {
-
+export async function scalewebsocket(data:Data){
+    
     const fetchedPrice = Number(data.p);
     const bidPrice = fetchedPrice + fetchedPrice * bidPriceIncrementRate;
     const askPrice = fetchedPrice - fetchedPrice * askPriceDecrementRate;
+   
 
     const symbol = data.s;
     const channel = symbol.replace("USDT","")
-    console.log("New price :",bidPrice, askPrice, symbol);
-    await pub.publish(channel,JSON.stringify({symbol,askPrice,bidPrice}));
-    console.log("published to Redis",bidPrice, askPrice);
-    await pub.hSet(`asset:${symbol}`,{
+     console.log("New price : ", bidPrice, askPrice,symbol);
+    await pub.publish(channel , JSON.stringify({symbol, askPrice , bidPrice})); //BTC is redis channel
+    console.log("Published to Redis", bidPrice, askPrice);
+    await pub.hSet(`asset:${symbol}`,{ //using the redis hash we can store the data in redis hash HSET automatically overwrites the field with the new value
         symbol,
-        askPrice,
-        bidPrice,
+        askPrice,   
+        bidPrice
     })
-    
 }
